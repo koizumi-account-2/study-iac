7bfb85188ca2e90002105fa8071fdcee6c595171
yositaka.koizumi <yositaka.koizumi@esco.co.jp>
2025-02-25 23:02:07 +0900

    CDK-演習2-環境ごとのdeploy

--- cdk/sqs_multi/bin/sqs_multi.ts
+++ cdk/sqs_multi/bin/sqs_multi.ts
@@ -3,20 +3,52 @@ import * as cdk from 'aws-cdk-lib';
 import { SqsMultiStack } from '../lib/sqs_multi-stack';
 import { environmentProps, Stages } from '../lib/environments';
 
-const app = new cdk.App();
 // 全てのスタックを記述する方法
-for (const stage of Object.keys(environmentProps) as Stages[]) {
-  const environment = environmentProps[stage];
-  const env: cdk.Environment = {
+// for (const stage of Object.keys(environmentProps) as Stages[]) {
+//   const environment = environmentProps[stage];
+//   const env: cdk.Environment = {
+//     account: environment.account,
+//     region: environment.region
+//   };
+//   new SqsMultiStack(app, `${stage}-SqsMultiStack1`, {
+//       env: env,
+//       queueName: `${stage}-queue-1`
+//   });
+//   new SqsMultiStack(app, `${stage}-SqsMultiStack2`, {
+//       env: env,
+//       queueName: `${stage}-queue-2`
+//   });
+// }
+
+// 環境ごとにスタックを記述する方法
+const stage = process.env.STAGE as Stages;
+if (!stage) {
+  throw new Error('STAGEが指定されていません');
+}
+const environment = environmentProps[stage];
+if (!environment) {
+  throw new Error('指定のSTAGEが存在しません');
+}
+
+// const env: cdk.Environment = {
+//   account: environment.account,
+//   region: environment.region
+// };
+
+const app = new cdk.App();
+
+// Stageコンストラクタ
+const st = new cdk.Stage(app, stage, {
+  env: {
     account: environment.account,
     region: environment.region
-  };
-  new SqsMultiStack(app, `${stage}-SqsMultiStack1`, {
-      env: env,
-      queueName: `${stage}-queue-1`
-  });
-  new SqsMultiStack(app, `${stage}-SqsMultiStack2`, {
-      env: env,
-      queueName: `${stage}-queue-2`
-  });
-}
+  }
+});
+
+new SqsMultiStack(st, 'SqsMultiStack1', {
+  queueName: `${stage}-queue-1`
+});
+
+new SqsMultiStack(st, 'SqsMultiStack2', {
+  queueName: `${stage}-queue-2`
+});
